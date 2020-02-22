521270005b37168aa5131ae0acc98661816fd526
Manuel Cañamero Fernández <manuel.spk@gmail.com>
2020-02-22 01:06:55 +0100

    Subiendo Proyecto

---
+++ Assets/Karting/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private string escenaACargar = "SampleScene";      //Nombre de la escena a cargar al pulsar el botón Play.
+
+
+
+    public void AlPulsarPlay()                             //Al pulsar el botón Play de la escena Main Menu
+    {
+        SceneManager.LoadScene(escenaACargar);
+    }
+
+    public void AlPulsarExit()                             //Al pulsar el botón Exit de la escena MainMenu
+    {
+        Application.Quit();                         //Cerramos el juego
+    }
+}
